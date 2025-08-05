d9c964e0c0dcd98df3e5c7463eaab2dc8fe2180e
gabrielvaletim-hue <gabriel.valetim@escola.pr.gov.br>
2025-08-05 11:02:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'plantio',
+    'para que serve o plantio',
+    'O plantio e feito para platar os grãos como soja e milho'
+)
+    
+
+criaCartao(
+    'colheita',
+    'Para que serve a colheita',
+    'a colheita serve para fazer a retirada dos grãos da lavoura'
+)
+
+criaCartao(
+    'Aplicação',
+    'Para que serve a aplicação ',
+    'A aplicação serve para fazer o controle de pragas e plantas invazoras, como buva e pé de galinha'
+)
+
+criaCartao(
+    'Preparo de solo',
+    'Como e feito o preparo de solo',
+    'Primeiro e feito uma mão de grade para tombar a terra depois outra mão de grade depois joga calcario'
+)
